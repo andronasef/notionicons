281901948f4de6c44c8c3d2b46d4732c800c4844
Andrew Nasef <androamazing@gmail.com>
2022-10-21 14:27:14 +0000

    footer fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,19 +5,18 @@
 <template>
   <footer class="text-center text-sm pt-8 pb-6">
     <p class="color-fade">
-      developed by
-      <a
-        class="opacity-75 hover:opacity-100"
-        href="https://github.com/antfu"
-        target="_blank"
-      >@antfu</a>,
-    <p class="color-fade">
-      customized by
+            customized by
       <a
         class="opacity-75 hover:opacity-100"
         href="https://github.com/andronasef"
         target="_blank"
       >@andronasef</a>,
+            developed by
+      <a
+        class="opacity-75 hover:opacity-100"
+        href="https://github.com/antfu"
+        target="_blank"
+      >@antfu</a>,
       powered by
       <a
         class="opacity-75 hover:opacity-100"
