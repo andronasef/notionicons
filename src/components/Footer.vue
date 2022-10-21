5adab176fd7c0100da22263f90c09a67ca8ce0e6
Andrew Nasef <41882346+andronasef@users.noreply.github.com>
2022-10-21 07:19:10 -0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,14 @@
 <template>
   <footer class="text-center text-sm pt-8 pb-6">
     <p class="color-fade">
-      forked by
+      developed by
+      <a
+        class="opacity-75 hover:opacity-100"
+        href="https://github.com/antfu"
+        target="_blank"
+      >@antfu</a>,
+    <p class="color-fade">
+      customized by
       <a
         class="opacity-75 hover:opacity-100"
         href="https://github.com/andronasef"
