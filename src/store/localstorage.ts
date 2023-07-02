60efcf70c283609e4934553e8a462fd86f71a3b8
Andrew Nasef <androamazing@gmail.com>
2023-07-02 22:40:24 +0000

    feat: change the default icon color

--- src/store/localstorage.ts
+++ src/store/localstorage.ts
@@ -1,73 +1,76 @@
-import type { IdCase } from '../utils/case'
-import { idCases } from '../utils/case'
+import type { IdCase } from '../utils/case';
+import { idCases } from '../utils/case';
 
-const RECENT_CAPACITY = 10
+const RECENT_CAPACITY = 10;
 
-export type ActiveMode = 'normal' | 'select' | 'copy'
+export type ActiveMode = 'normal' | 'select' | 'copy';
 
-export const themeColor = "#9c5bd9"
-export const iconSize = useStorage('icones-icon-size', 'text-4xl')
-export const previewColor = ref('#888888')
-export const copyPreviewColor = useStorage('icones-copy-preview-color', true)
-export const listType = useStorage('icones-list-type', 'grid')
-export const favoritedIds = useStorage<string[]>('icones-fav-collections', [])
-export const recentIds = useStorage<string[]>('icones-recent-collections', [])
-export const bags = useStorage<string[]>('icones-bags', [])
-export const activeMode = useStorage<ActiveMode>('active-mode', 'normal')
-export const preferredCase = useStorage<IdCase>('icones-preferfed-case', 'iconify')
-export const sortAlphabetically = useStorage('icones-alpha-sort-collections', false)
+export const themeColor = '#9c5bd9';
+export const iconSize = useStorage('icones-icon-size', 'text-4xl');
+export const previewColor = ref('##D3D3D3');
+export const copyPreviewColor = useStorage('icones-copy-preview-color', true);
+export const listType = useStorage('icones-list-type', 'grid');
+export const favoritedIds = useStorage<string[]>('icones-fav-collections', []);
+export const recentIds = useStorage<string[]>('icones-recent-collections', []);
+export const bags = useStorage<string[]>('icones-bags', []);
+export const activeMode = useStorage<ActiveMode>('active-mode', 'normal');
+export const preferredCase = useStorage<IdCase>(
+  'icones-preferfed-case',
+  'iconify'
+);
+export const sortAlphabetically = useStorage(
+  'icones-alpha-sort-collections',
+  false
+);
 
 export function getTransformedId(icon: string) {
-  return idCases[preferredCase.value]?.(icon) || icon
+  return idCases[preferredCase.value]?.(icon) || icon;
 }
 
 export function isFavorited(id: string) {
-  return favoritedIds.value.includes(id)
+  return favoritedIds.value.includes(id);
 }
 
 export function isRecent(id: string) {
-  return recentIds.value.includes(id)
+  return recentIds.value.includes(id);
 }
 
 export function pushRecent(id: string) {
-  recentIds.value = [id, ...recentIds.value.filter(i => i !== id)].slice(0, RECENT_CAPACITY)
+  recentIds.value = [id, ...recentIds.value.filter((i) => i !== id)].slice(
+    0,
+    RECENT_CAPACITY
+  );
 }
 
 export function removeRecent(id: string) {
-  recentIds.value = recentIds.value.filter(i => i !== id)
+  recentIds.value = recentIds.value.filter((i) => i !== id);
 }
 
 export function toggleFavorite(id: string) {
-  const index = favoritedIds.value.indexOf(id)
-  if (index >= 0)
-    favoritedIds.value.splice(index, 1)
-  else
-    favoritedIds.value.push(id)
+  const index = favoritedIds.value.indexOf(id);
+  if (index >= 0) favoritedIds.value.splice(index, 1);
+  else favoritedIds.value.push(id);
 }
 
 export function addToBag(id: string) {
-  if (!bags.value.includes(id))
-    bags.value.push(id)
+  if (!bags.value.includes(id)) bags.value.push(id);
 }
 
 export function removeFromBag(id: string) {
-  const index = bags.value.indexOf(id)
-  if (index >= 0)
-    bags.value.splice(index, 1)
+  const index = bags.value.indexOf(id);
+  if (index >= 0) bags.value.splice(index, 1);
 }
 
 export function inBag(id: string) {
-  return bags.value.includes(id)
+  return bags.value.includes(id);
 }
 
 export function toggleBag(id: string) {
-  const index = bags.value.indexOf(id)
-  if (index >= 0)
-    bags.value.splice(index, 1)
-  else
-    bags.value.push(id)
+  const index = bags.value.indexOf(id);
+  if (index >= 0) bags.value.splice(index, 1);
+  else bags.value.push(id);
 }
 
 export function clearBag() {
-  bags.value = []
+  bags.value = [];
 }
